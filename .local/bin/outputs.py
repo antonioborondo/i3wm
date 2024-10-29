186f8c994455600d5792275c70fa0b1ae925dd12
Antonio Borondo <abo@mega.co.nz>
2024-10-29 20:11:26 +0100

    Add script to get the outputs

---
+++ .local/bin/outputs.py
@@ -0,0 +1,111 @@
+from dataclasses import dataclass
+from enum import Enum
+from typing import List, Optional
+import re
+import subprocess
+
+
+@dataclass
+class Resolution:
+    x: int
+    y: int
+
+
+@dataclass
+class Size:
+    x: int
+    y: int
+
+
+class Type(Enum):
+    LAPTOP = 1
+    MONITOR = 2
+
+
+class Status(Enum):
+    DISABLED = 1
+    ENABLED = 2
+
+
+class Dpi(Enum):
+    NORMAL = 96
+    HIGH = 192
+
+
+@dataclass
+class Output:
+    id: str = ""
+    resolution: Resolution = None
+    size: Optional[Size] = None
+
+    def type(self) -> Type:
+        if self.id == "eDP-1":
+            return Type.LAPTOP
+        else:
+            return Type.MONITOR
+
+    def status(self) -> Status:
+        if self.size == None:
+            return Status.DISABLED
+        else:
+            return Status.ENABLED
+
+    def dpi(self) -> Dpi:
+        if self.resolution and self.size:
+            size_x_in_cm = self.size.x / 10
+            size_x_in_in = size_x_in_cm / 2.54
+            dpi_x = self.resolution.x / size_x_in_in
+
+            size_y_in_cm = self.size.y / 10
+            size_y_in_in = size_y_in_cm / 2.54
+            dpi_y = self.resolution.y / size_y_in_in
+
+            if max(dpi_x, dpi_y) > 150:
+                return Dpi.HIGH
+            else:
+                return Dpi.NORMAL
+        else:
+            return Dpi.NORMAL
+
+    def enable(self):
+        # Set the DPI with xrdb to ensure the mouse and GTK apps are displayed correctly.
+        # Note: GTK apps need to be restarted for the changes to take effect.
+        command = f'echo "Xft.dpi: {self.dpi().value}" | xrdb -merge'
+        subprocess.run(command, shell=True)
+
+        # Set the DPI with xrandr to ensure i3 UI elements are displayed correctly.
+        command = f"xrandr --output {self.id} --dpi {self.dpi().value} --mode {self.resolution.x}x{self.resolution.y} --pos 0x0 --primary --rotate normal"
+        subprocess.run(command, shell=True)
+
+    def disable(self):
+        command = f"xrandr --output {self.id} --off"
+        subprocess.run(command, shell=True)
+
+
+def get() -> List[Output]:
+    command = "xrandr | grep -A 1 --no-group-separator -w connected"
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    lines = result.stdout.strip().split("\n")
+
+    outputs: List[Output] = []
+    for i in range(0, len(lines) - 1, 2):
+
+        output = Output()
+
+        id_match = re.search(r"^(.+)\sconnected\s.*$", lines[i])
+        output.id = id_match.group(1)
+
+        size_match = re.search(r"^.*\s([0-9]+)mm\sx\s([0-9]+)mm$", lines[i])
+        if size_match:
+            output.size = Size(int(size_match.group(1)), int(size_match.group(2)))
+
+        resolution_match = re.search(
+            r"^\s+([0-9]+)x([0-9]+)\s+[0-9]+\.[0-9]+(?:\*|\s)\+$", lines[i + 1]
+        )
+        output.resolution = Resolution(
+            int(resolution_match.group(1)), int(resolution_match.group(2))
+        )
+
+        outputs.append(output)
+
+    return outputs
