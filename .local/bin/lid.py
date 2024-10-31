776367954e4008cdf425c75dd13086d330994af8
Antonio Borondo <abo@mega.co.nz>
2024-10-31 13:17:00 +0100

    Create Lid class

--- .local/bin/lid.py
+++ .local/bin/lid.py
@@ -8,24 +8,25 @@ class Status(Enum):
     OPEN = "open"
 
 
-def status() -> Status:
-    try:
-        command = [
-            "sh",
-            "-c",
-            "cat /proc/acpi/button/lid/LID0/state | awk '{print $2}'",
-        ]
-        command_result = subprocess.run(
-            command, capture_output=True, check=True, text=True
-        )
-        command_output = command_result.stdout.strip()
+class Lid:
+    def status(self) -> Status:
+        try:
+            command = [
+                "sh",
+                "-c",
+                "cat /proc/acpi/button/lid/LID0/state | awk '{print $2}'",
+            ]
+            command_result = subprocess.run(
+                command, capture_output=True, check=True, text=True
+            )
+            command_output = command_result.stdout.strip()
 
-        if command_output == Status.CLOSED.value:
-            return Status.CLOSED
-        elif command_output == Status.OPEN.value:
-            return Status.OPEN
-        else:
+            if command_output == Status.CLOSED.value:
+                return Status.CLOSED
+            elif command_output == Status.OPEN.value:
+                return Status.OPEN
+            else:
+                return Status.UNKNOWN
+
+        except Exception:
             return Status.UNKNOWN
-
-    except Exception:
-        return Status.UNKNOWN
